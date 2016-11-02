12feb3be53a822b5d2bc58350a27c3710209d1a9
Nagaraju <nvarthala@logitech.com>
2016-11-02 12:19:19 +0530

    Removed build folder

--- build/Debug-iphonesimulator/Pods_VIPER_Demo.framework/Headers/Pods-VIPER Demo-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_VIPER_DemoVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_VIPER_DemoVersionString[];
-
--- build/Pods.build/Debug-iphonesimulator/Pods-VIPER Demo.build/DerivedSources/Pods_VIPER_Demo_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_VIPER_DemoVersionString[];
- extern const double Pods_VIPER_DemoVersionNumber;
-
- const unsigned char Pods_VIPER_DemoVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_VIPER_Demo  PROJECT:Pods-1" "\n";
- const double Pods_VIPER_DemoVersionNumber __attribute__ ((used)) = (double)1.;
